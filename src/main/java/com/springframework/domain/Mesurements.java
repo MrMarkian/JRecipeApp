15c9e2fc9646095705538d1e14d805845a4dd48f
Markian Rutkowskyj <i-keep-laughing@myself.com>
2020-07-28 01:24:06 +0100

    Completing assignment

---
+++ src/main/java/com/springframework/domain/Mesurements.java
@@ -0,0 +1,30 @@
+package com.springframework.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Mesurements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String unitOfMeasure;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUnitOfMeasure() {
+        return unitOfMeasure;
+    }
+
+    public void setUnitOfMeasure(String unitOfMeasure) {
+        this.unitOfMeasure = unitOfMeasure;
+    }
+}
