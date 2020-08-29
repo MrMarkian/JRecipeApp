479bd5ee64539ebc94b53f6fd266f566aad39cd2
MrMarkian <43710879+MrMarkian@users.noreply.github.com>
2020-08-30 00:03:46 +0100

    Added Ingredient deletion

--- src/main/java/com/springframework/commands/RecipeCommand.java
+++ src/main/java/com/springframework/commands/RecipeCommand.java
@@ -1,6 +1,7 @@
 package com.springframework.commands;
 
 import com.springframework.domain.Difficulty;
+import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,8 +9,7 @@ import lombok.Setter;
 import java.util.HashSet;
 import java.util.Set;
 
-@Setter
-@Getter
+@Data
 @NoArgsConstructor
 public class RecipeCommand {
 
